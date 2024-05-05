dea302c3dded220a91317375614541bc0c84dfbe
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-05-05 20:37:58 +0200

    add autocmd in setup function

--- lua/winbar/init.lua
+++ lua/winbar/init.lua
@@ -1,8 +1,9 @@
 local utils = require("winbar.utils")
+local vim = vim
 local M = {}
 
-local is_empty = function(s)
-	return s == nil or s == ""
+local function augroup(name)
+	return vim.api.nvim_create_augroup("winbar_" .. name, { clear = true })
 end
 
 local get_buf_option = function(opt)
@@ -39,7 +40,45 @@ end
 
 function M.setup()
 	print("HEllo world")
-end
 
+	vim.api.nvim_create_autocmd({ "BufEnter", "VimEnter" }, {
+		group = augroup("status_line"),
+		callback = function()
+			local winbar_filetype_exclude = {
+				"help",
+				"startify",
+				"dashboard",
+				"packer",
+				"neo-tree",
+				"neogitstatus",
+				"NvimTree",
+				"Trouble",
+				"alpha",
+				"lir",
+				"Outline",
+				"spectre_panel",
+				"toggleterm",
+				"TelescopePrompt",
+				"prompt",
+			}
+
+			if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
+				vim.opt_local.winbar = nil
+				return
+			end
+
+			local win_number = vim.api.nvim_get_current_win()
+			local config = vim.api.nvim_win_get_config(win_number)
+
+			if config.relative == "" then
+				vim.opt_local.winbar = " " .. utils.get_winbar()
+				-- vim.opt_local.statuscolumn = require("user.statuscolumn")
+			else
+				vim.opt_local.winbar = nil
+			end
+			-- vim.wo.winhighlight = 'Normal:NvimSeparator'
+		end,
+	})
+end
 
 return M
